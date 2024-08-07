37226ffebd04a1e1a618debf5822c918649e6d7b
Nguyễn Hồng Sơn <sonnh.it2@vnpt.vn>
2024-08-07 14:47:31 +0700

    dieu chinh logic ham tao moi nguoi dung

--- src/services/userService.js
+++ src/services/userService.js
@@ -116,24 +116,24 @@ let createNewUser = (data) =>{
                     errCode: 1,
                     errMessage: 'Your email is existed. Please try another email'
                 });                
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender==='1'?true:false,
+                    roleId: data.roleId
+                });
+    
+                resolve({
+                    errCode: 0,
+                    message: "Ok"
+                });
             }
-
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender==='1'?true:false,
-                roleId: data.roleId
-            });
-
-            resolve({
-                errCode: 0,
-                message: "Ok"
-            });
         } catch (e) {
             reject(e);
         }
